696dbfa89dc9419796bc318375d048066577c320
hsji0 <76461675+hsji0@users.noreply.github.com>
2021-06-24 14:24:15 +0900

    Update dataset.py

--- core/dataset.py
+++ core/dataset.py
@@ -10,10 +10,13 @@ import core.utils as utils
 from core.config import cfg
 
 
+
+
 class Dataset(object):
     """implement Dataset here"""
 
     def __init__(self, FLAGS, input_channel, is_training: bool, dataset_type: str = "yolo"):
+
         self.input_channel = input_channel
         self.strides, self.anchors, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
         self.dataset_type = dataset_type
@@ -39,67 +42,58 @@ class Dataset(object):
         self.num_samples = len(self.annotations)
         self.num_batchs = int(np.ceil(self.num_samples / self.batch_size))
         self.batch_count = 0
-        self.mosaic = True
 
-    def mosaic_aug(self, image, bboxes, xrange = [0.3, 0.7] , yrange = [0.3, 0.7] ):
-        image_size = cfg.TRAIN.INPUT_SIZE
-        output_image = np.zeros((image_size, image_size, 3), dtype=np.float32)
-        xratio = xrange[0] + (xrange[1] - xrange[0]) * random.random()
-        neww = int(image_size * xratio)
-        yratio = yrange[0] + (yrange[1] - yrange[0]) * random.random()
-        newh = int(image_size * yratio)
+        # self.mosaic = True
 
-        # print("@@@ image shape:{}".format(image.shape))        # (640, 640, 3)
-        # print("@@@ bboxes shape:{}".format(bboxes.shape))      # (8, 5)
-        if len(image) < 4:
-            # print("batch size < 4,skip mosaic")
-            return image, bboxes
-
-
-        # cv2.imshow("111resize",  cv2.resize(image[0], (neww, newh)))
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        # print("output_image shape:{}".format(output_image.shape))
-        # print("neww newh shape:{} {}".format(neww, newh))
-        # print("bboxes[0,:,0] {}".format(bboxes[0]))
-        mosaic_bboxes = []
-        for img_idx, bboxes_per_img in enumerate(bboxes):
-            mosaic_bboxes_per_img = []
-            for bbox_idx, bbox in enumerate(bboxes_per_img):
-                # print("bbox:{}".format(bbox))
-                bbox = np.array(bbox).astype(np.float32)
-                if img_idx == 0:  # top left
-                    output_image[:newh, :neww, :] = cv2.resize(image[0], (neww, newh))
-                    bbox[...,0] *= xratio
-                    bbox[...,1] *= yratio
-                    bbox[...,2] *= xratio
-                    bbox[...,3] *= yratio
-                elif img_idx == 1:  # top right
-                    output_image[:newh, neww:, :] = cv2.resize(image[1], (image_size - neww, newh))
-                    bbox[..., 0] = bbox[..., 2] + bbox[..., 0] * (1-xratio)
-                    bbox[..., 1] *= yratio
-                    bbox[..., 2] *= (1-xratio)
-                    bbox[..., 3] *= yratio
-                elif img_idx == 2:  # bottom left
-                    output_image[newh:, :neww, :] = cv2.resize(image[2], (neww, image_size - newh))
-                    bbox[..., 0] *= xratio
-                    bbox[..., 1] *= bbox[..., 3] + bbox[..., 1] * (1-yratio)
-                    bbox[..., 2] *= xratio
-                    bbox[..., 3] *= (1-yratio)
-                else:
-                    output_image[newh:, neww:, :] = cv2.resize(image[3], (image_size - neww, image_size - newh))
-                    bbox[..., 0] = bbox[..., 2] + bbox[..., 0] * (1 - xratio)
-                    bbox[..., 1] = bbox[..., 3] + bbox[..., 1] * (1 - yratio)
-                    bbox[..., 2] = bbox[..., 2] * (1 - xratio)
-                    bbox[..., 3] = bbox[..., 3] * (1 - yratio)
-                mosaic_bboxes_per_img.append(bbox)
-            mosaic_bboxes.append(mosaic_bboxes_per_img)
+    # def mosaic_aug(self, image, bboxes, xrange = [0.3, 0.7] , yrange = [0.3, 0.7] ):
+    #     image_size = cfg.TRAIN.INPUT_SIZE
+    #     output_image = np.zeros((image_size, image_size, 3), dtype=np.float32)
+    #     xratio = xrange[0] + (xrange[1] - xrange[0]) * random.random()
+    #     neww = int(image_size * xratio)
+    #     yratio = yrange[0] + (yrange[1] - yrange[0]) * random.random()
+    #     newh = int(image_size * yratio)
+    #
+    #     if len(image) < 4:
+    #         return image, bboxes
+    #
+    #     mosaic_bboxes = []
+    #     for img_idx, bboxes_per_img in enumerate(bboxes):
+    #         mosaic_bboxes_per_img = []
+    #         for bbox_idx, bbox in enumerate(bboxes_per_img):
+    #             # print("bbox:{}".format(bbox))
+    #             bbox = np.array(bbox).astype(np.float32)
+    #             if img_idx == 0:  # top left
+    #                 output_image[:newh, :neww, :] = cv2.resize(image[0], (neww, newh))
+    #                 bbox[...,0] *= xratio
+    #                 bbox[...,1] *= yratio
+    #                 bbox[...,2] *= xratio
+    #                 bbox[...,3] *= yratio
+    #             elif img_idx == 1:  # top right
+    #                 output_image[:newh, neww:, :] = cv2.resize(image[1], (image_size - neww, newh))
+    #                 bbox[..., 0] = bbox[..., 2] + bbox[..., 0] * (1-xratio)
+    #                 bbox[..., 1] *= yratio
+    #                 bbox[..., 2] *= (1-xratio)
+    #                 bbox[..., 3] *= yratio
+    #             elif img_idx == 2:  # bottom left
+    #                 output_image[newh:, :neww, :] = cv2.resize(image[2], (neww, image_size - newh))
+    #                 bbox[..., 0] *= xratio
+    #                 bbox[..., 1] *= bbox[..., 3] + bbox[..., 1] * (1-yratio)
+    #                 bbox[..., 2] *= xratio
+    #                 bbox[..., 3] *= (1-yratio)
+    #             else:
+    #                 output_image[newh:, neww:, :] = cv2.resize(image[3], (image_size - neww, image_size - newh))
+    #                 bbox[..., 0] = bbox[..., 2] + bbox[..., 0] * (1 - xratio)
+    #                 bbox[..., 1] = bbox[..., 3] + bbox[..., 1] * (1 - yratio)
+    #                 bbox[..., 2] = bbox[..., 2] * (1 - xratio)
+    #                 bbox[..., 3] = bbox[..., 3] * (1 - yratio)
+    #             mosaic_bboxes_per_img.append(bbox)
+    #         mosaic_bboxes.append(mosaic_bboxes_per_img)
 
         # cv2.imshow("MOSAIC AUG", output_image)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
 
-        return output_image, mosaic_bboxes
+        # return output_image, mosaic_bboxes
 
     def load_annotations(self):
         with open(self.annot_path, "r") as f:
@@ -115,12 +109,14 @@ class Dataset(object):
                 for line in txt:
                     image_path = line.strip()
                     root, _ = os.path.splitext(image_path)
+
                     with open(root + ".txt") as fd:
                         boxes = fd.readlines()
                         string = ""
                         for box in boxes:
                             box = box.strip()
                             box = box.split()
+
                             if len(box) > 4:
                                 class_num = int(box[0])
                                 center_x = float(box[1])
@@ -143,7 +139,7 @@ class Dataset(object):
         return self
 
     def __next__(self):
-        with tf.device("/cpu:0"):
+        with tf.device("/gpu:0"):
             # self.train_input_size = random.choice(self.train_input_sizes)
             self.num_detection_layers = cfg.YOLO.NUM_YOLOLAYERS
             self.train_input_size = cfg.TRAIN.INPUT_SIZE
@@ -163,7 +159,6 @@ class Dataset(object):
             batch_label_mbbox = 0
             batch_label_lbbox = 0
 
-            # for mosaic aug
             self.bboxes_list = []
 
             if self.num_detection_layers == 3:
@@ -233,13 +228,13 @@ class Dataset(object):
             )
 
             num = 0
-            # num_mosaic = min(self.num_batchs // 4
             if self.batch_count < self.num_batchs:
                 while num < self.batch_size:
                     index = self.batch_count * self.batch_size + num
                     if index >= self.num_samples:
                         index -= self.num_samples
                     annotation = self.annotations[index]
+
                     if cfg.YOLO.NUM_YOLOLAYERS == 3:
                         image, bboxes = self.parse_annotation(annotation)
                         (
@@ -275,30 +270,36 @@ class Dataset(object):
                         num += 1
                     else:
                         image, bboxes = self.parse_annotation(annotation)
-                        # print("@@@\n bboxes shape:{}".format(bboxes.shape))   # (BBOXES NUM, 5)
+
                         (
                             label_mbbox,
                             mbboxes,
                         ) = self.preprocess_true_boxes(bboxes, self.num_detection_layers)
 
-                        batch_image[num, :, :, :] = image
+                        batch_image[num:num+1, :, :, :] = image
                         batch_label_mbbox[num:num+1, :, :, :, :] = label_mbbox
                         batch_mbboxes[num:num+1, :, :] = mbboxes
                         num += 1
 
-                        if self.mosaic == True and max(1, num) % 4 == 0:
-                            if random.randint(0,1) == 0:  # 50% 확률
-                                mosaic_imgs = []
-                                mosaic_bboxes = []
-                                picked_indexes = random.sample(range(num-4, num), 4)
-                                # print("picked indexes shape:{}".format(picked_indexes))
-                                # print("batch_image shape:{}".format(np.array(batch_image).shape))
 
-                                for picked_idx in picked_indexes:
-                                    mosaic_imgs.append(batch_image[picked_idx:picked_idx+1,...])
-                                    mosaic_bboxes.append(np.array(self.bboxes_list)[picked_idx:picked_idx+1,...])
-                                # print("mosaic_bboxes shape:{}".format(np.array(mosaic_bboxes).shape))
-                                image, bboxes = self.mosaic_aug(np.concatenate(mosaic_imgs, axis=0), np.concatenate(mosaic_bboxes, axis=0))
+
+                        # cv2.imshow(image, "image_222")
+                        # cv2.waitKey(0)
+                        # cv2.destroyAllWindows()
+
+                        # if self.mosaic == True and max(1, num) % 4 == 0:
+                        #     if random.randint(0,1) == 0:  # 50% 확률
+                        #         mosaic_imgs = []
+                        #         mosaic_bboxes = []
+                        #         picked_indexes = random.sample(range(num-4, num), 4)
+                        #         # print("picked indexes shape:{}".format(picked_indexes))
+                        #         # print("batch_image shape:{}".format(np.array(batch_image).shape))
+                        #
+                        #         for picked_idx in picked_indexes:
+                        #             mosaic_imgs.append(batch_image[picked_idx:picked_idx+1,...])
+                        #             mosaic_bboxes.append(np.array(self.bboxes_list)[picked_idx:picked_idx+1,...])
+                        #         # print("mosaic_bboxes shape:{}".format(np.array(mosaic_bboxes).shape))
+                        #         image, bboxes = self.mosaic_aug(np.concatenate(mosaic_imgs, axis=0), np.concatenate(mosaic_bboxes, axis=0))
                 self.batch_count += 1
 
 
@@ -326,6 +327,7 @@ class Dataset(object):
                     )
                 elif cfg.YOLO.NUM_YOLOLAYERS == 1:
                     batch_target = batch_label_mbbox, batch_mbboxes
+
                     return (
                         batch_image,
                         (
@@ -390,7 +392,7 @@ class Dataset(object):
 
             bboxes[:, [0, 2]] = bboxes[:, [0, 2]] - crop_xmin
             bboxes[:, [1, 3]] = bboxes[:, [1, 3]] - crop_ymin
-        print("image shape:{}".format(image.shape))
+
         return image, bboxes
 
     def random_translate(self, image, bboxes):
@@ -433,8 +435,8 @@ class Dataset(object):
 
     def parse_annotation(self, annotation):
         line = annotation.split()
-        # print("line check:{}".format(line[:3]))
         image_path = line[0]
+
         if not os.path.exists(image_path):
             raise KeyError("%s does not exist ... " % image_path)
         image = cv2.imread(image_path)
@@ -449,7 +451,7 @@ class Dataset(object):
             )
             bboxes = bboxes * np.array([width, height, width, height, 1])  # <x_center> <y_center> <width> <height>
             bboxes = bboxes.astype(np.int64)
-            # print("@@@ bboxes content check:{}".format(bboxes))
+
         if self.data_aug:
             image, bboxes = self.random_horizontal_flip(
                 np.copy(image), np.copy(bboxes)
@@ -457,20 +459,17 @@ class Dataset(object):
             image, bboxes = self.random_crop(
                 np.copy(image), np.copy(bboxes)
             )
-            print("@@@ image shape(crop):{}".format(image.shape))
             image, bboxes = self.random_translate(
                 np.copy(image), np.copy(bboxes)
             )
-            print("@@@ image shape(trans):{}".format(image.shape))
+
             image = self.random_color(
                 np.copy(image)
             )
-            print("@@@ image shape(color):{}".format(image.shape))
-            if self.mosaic == True:
-                self.bboxes_list.append(bboxes)
 
+            # if self.mosaic == True:
+            #     self.bboxes_list.append(bboxes)
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image, bboxes = utils.image_preprocess(
             np.copy(image),
             [self.train_input_size, self.train_input_size],
@@ -479,7 +478,6 @@ class Dataset(object):
         return image, bboxes
 
     def preprocess_true_boxes(self, bboxes, num_detection_layers):
-        print("preprocess @!@: {}".format(np.array(bboxes).shape))
         label = [
             np.zeros(
                 (
@@ -491,9 +489,20 @@ class Dataset(object):
             )
             for i in range(num_detection_layers)
         ]
+
+        count_overlap = [
+            np.zeros(
+                (
+                    self.train_output_sizes[i],
+                    self.train_output_sizes[i],
+                )
+                , dtype=np.int32
+            )
+            for i in range(num_detection_layers)
+        ]
+
         bboxes_xywh = [np.zeros((self.max_bbox_per_scale, 4)) for _ in range(num_detection_layers)]
         bbox_count = np.zeros((num_detection_layers,))
-        print(np.array(bboxes_xywh).shape)
 
         for bbox in bboxes:
             bbox_coor = bbox[:4]
@@ -507,7 +516,6 @@ class Dataset(object):
             deta = 0.01
             smooth_onehot = onehot * (1 - deta) + deta * uniform_distribution
 
-            # print("before concat ", bbox_xywh)
             bbox_xywh = np.concatenate(
                 [
                     (bbox_coor[2:] + bbox_coor[:2]) * 0.5,
@@ -515,13 +523,11 @@ class Dataset(object):
                 ],
                 axis=-1,
             )
-            print("bbox_xywh:{} shape:{}".format(bbox_xywh, bbox_xywh.shape))
 
 
             bbox_xywh_scaled = (
                 1.0 * bbox_xywh[np.newaxis, :] / self.strides[:, np.newaxis]
             )
-            print("bbox_xywh_scaled:{} shape:{}".format(bbox_xywh_scaled, np.array(bbox_xywh_scaled).shape))  # (1,4)
 
             iou = []
             exist_positive = False
@@ -530,25 +536,32 @@ class Dataset(object):
                 anchors_xywh[:, 0:2] = (
                     np.floor(bbox_xywh_scaled[i, 0:2]).astype(np.int32) + 0.5
                 )
-                print("anchors xy",np.array(anchors_xywh[:, 0:2]).shape)
-                anchors_xywh[:, 2:4] = self.anchors[i]
-                print("bbox_xywh_scaled[i, 0:2]", np.array(bbox_xywh_scaled[i, 0:2]).shape)  # bbox_xywh_scaled[i, 0:2] (2,)
-                print("anchors_wh", self.anchors[i])
+                # anchors_xywh[:, 2:4] = self.anchors[i]
+                anchors_xywh[:, 2:4] = self.anchors[i] / cfg.YOLO.STRIDES_CUSTOM[i]
+
                 iou_scale = utils.bbox_iou(
                     bbox_xywh_scaled[i][np.newaxis, :], anchors_xywh
                 )
                 iou.append(iou_scale)
                 iou_mask = iou_scale > 0.3
 
+
                 if np.any(iou_mask):
                     xind, yind = np.floor(bbox_xywh_scaled[i, 0:2]).astype(
                         np.int32
                     )
 
-                    label[i][yind, xind, iou_mask, :] = 0
-                    label[i][yind, xind, iou_mask, 0:4] = bbox_xywh
-                    label[i][yind, xind, iou_mask, 4:5] = 1.0
-                    label[i][yind, xind, iou_mask, 5:] = smooth_onehot
+
+                    label_idx = count_overlap[i][yind, xind]
+                    # label[i][yind, xind, iou_mask, :] = 0
+                    # label[i][yind, xind, iou_mask, 0:4] = bbox_xywh
+                    # label[i][yind, xind, iou_mask, 4:5] = 1.0
+                    # label[i][yind, xind, iou_mask, 5:] = smooth_onehot
+                    label[i][yind, xind, label_idx:label_idx+1, :] = 0
+                    label[i][yind, xind, label_idx:label_idx+1, 0:4] = bbox_xywh
+                    label[i][yind, xind, label_idx:label_idx+1, 4:5] = 1.0
+                    label[i][yind, xind, label_idx:label_idx+1, 5:] = smooth_onehot
+                    count_overlap[i][yind, xind] += 1   # 한 grid 에 중심점 3개 초과시 에러날 것
 
                     bbox_ind = int(bbox_count[i] % self.max_bbox_per_scale)
                     bboxes_xywh[i][bbox_ind, :4] = bbox_xywh
@@ -557,20 +570,27 @@ class Dataset(object):
                     exist_positive = True
 
             if not exist_positive:
-                print("iou", iou)
                 best_anchor_ind = np.argmax(np.array(iou).reshape(-1), axis=-1)
-                print("best_anchor_ind", best_anchor_ind)
 
                 best_detect = int(best_anchor_ind / self.anchor_per_scale)
+
                 best_anchor = int(best_anchor_ind % self.anchor_per_scale)
                 xind, yind = np.floor(
                     bbox_xywh_scaled[best_detect, 0:2]
                 ).astype(np.int32)
 
-                label[best_detect][yind, xind, best_anchor, :] = 0
-                label[best_detect][yind, xind, best_anchor, 0:4] = bbox_xywh
-                label[best_detect][yind, xind, best_anchor, 4:5] = 1.0
-                label[best_detect][yind, xind, best_anchor, 5:] = smooth_onehot
+                label_idx = count_overlap[best_detect][yind, xind]
+                label[best_detect][yind, xind, label_idx:label_idx+1, :] = 0
+                label[best_detect][yind, xind, label_idx:label_idx+1, 0:4] = bbox_xywh
+                label[best_detect][yind, xind, label_idx:label_idx+1, 4:5] = 1.0
+                label[best_detect][yind, xind, label_idx:label_idx+1, 5:] = smooth_onehot
+                count_overlap[best_detect][yind, xind] += 1  # 한 grid 에 중심점 3개 초과시 에러날 것
+
+                ## 원래
+                # label[best_detect][yind, xind, best_anchor, :] = 0
+                # label[best_detect][yind, xind, best_anchor, 0:4] = bbox_xywh
+                # label[best_detect][yind, xind, best_anchor, 4:5] = 1.0
+                # label[best_detect][yind, xind, best_anchor, 5:] = smooth_onehot
 
                 bbox_ind = int(
                     bbox_count[best_detect] % self.max_bbox_per_scale
